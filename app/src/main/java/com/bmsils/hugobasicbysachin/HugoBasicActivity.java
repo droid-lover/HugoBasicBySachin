29f59a23b2675fe842b37239e0f5299797291f26
myJarvis <ersachin20@gmail.com>
2016-08-12 12:54:55 +0530

    first commit

---
+++ app/src/main/java/com/bmsils/hugobasicbysachin/HugoBasicActivity.java
@@ -0,0 +1,68 @@
+package com.bmsils.hugobasicbysachin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import hugo.weaving.DebugLog;
+
+public class HugoBasicActivity extends AppCompatActivity {
+
+    @Bind(R.id.hugoBasicActivityToolbar)
+    Toolbar hugoBasicActivityToolbar;
+
+    @Bind(R.id.hugoBasicActivityFirstNumberTILEditText)
+    TextInputEditText hugoBasicActivityFirstNumberTILEditText;
+
+    @Bind(R.id.hugoBasicActivitySecondNumberTILEditText)
+    TextInputEditText hugoBasicActivitySecondNumberTILEditText;
+
+    @Bind(R.id.hugoBasicActivityAddButton)
+    Button hugoBasicActivityAddButton;
+
+    int num1, num2 = 0;
+
+    int sum = 0;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hugo_basic);
+        ButterKnife.bind(this);
+        setSupportActionBar(hugoBasicActivityToolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "This Project is the property of Sachin", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.hugoBasicActivityAddButton)
+    public void hugoBasicActivityAddButtonClicked() {
+
+        num1 = Integer.parseInt(hugoBasicActivityFirstNumberTILEditText.getText().toString());
+        num2 = Integer.parseInt(hugoBasicActivitySecondNumberTILEditText.getText().toString());
+        getSum(num1, num2);
+    }
+
+    @DebugLog
+    private int getSum(int firstNum, int secondNum) {
+        sum = firstNum + secondNum;
+        return sum;
+    }
+
+}
